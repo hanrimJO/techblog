d10f11612afc8fe709793d5615736731eb891772
hanrimJO <whgksfla@gmail.com>
2020-05-30 21:11:56 +0900

    blog: Add create Post and Update Post test code

--- blog/tests.py
+++ blog/tests.py
@@ -44,3 +44,59 @@ class TestModel(TestCase):
         )
         self.assertEqual(category.post_set.count(), 1)
 
+    def test_post(self):
+        category = create_category()
+        post_000 = create_post(
+            title='the first post',
+            content='Hello World',
+            author=self.author_000,
+            created=timezone.now(),
+            modified=timezone.now(),
+            category=category
+        )
+
+
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.author_000 = User.objects.create_user(username='hanrim', password='nopassword')
+
+    def test_post_create(self):
+        category = create_category()
+        post_000 = create_post(
+            title='the first post',
+            content='Hello World',
+            author=self.author_000,
+            created=timezone.now(),
+            modified=timezone.now(),
+            category=category
+        )
+        self.assertEqual(Post.objects.all().count(), 1)
+
+
+    def test_post_update(self):
+        category = create_category()
+        time_now = timezone.now()
+        post_000 = create_post(
+            title='the first post',
+            content='Hello World',
+            author=self.author_000,
+            created=time_now,
+            modified=time_now,
+            category=category
+        )
+        print(post_000.created, post_000.modified)
+        self.assertEqual(Post.objects.all().count(), 1)
+        self.assertEqual('the first post', post_000.title)
+
+        post_000.title = 'hihihi'
+        post_000.save()
+
+        print(post_000.created, post_000.modified)
+        self.assertEqual('hihihi', post_000.title)
+
+
+
+
+
+
