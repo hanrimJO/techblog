1d5b8bb3eea6eea9f67bc8f0f8cfb091c704e93b
hanrimJO <whgksfla@gmail.com>
2020-06-01 14:32:48 +0900

    blog: Add postlist and category test code

--- blog/tests.py
+++ blog/tests.py
@@ -61,6 +61,53 @@ class TestView(TestCase):
         self.client = Client()
         self.author_000 = User.objects.create_user(username='hanrim', password='nopassword')
 
+    def test_post_list_no_post(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        main_div = soup.find('div', id='main-div')
+
+        self.assertIn('게시물이 아직 없습니다', main_div.text)
+
+    def test_post_list_post(self):
+        post_000 = create_post(
+            title='the first post',
+            content='Hello World',
+            author=self.author_000,
+            created=timezone.now(),
+            modified=timezone.now(),
+        )
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        main_div = soup.find('div', id='main-div')
+
+        self.assertNotIn('게시물이 아직 없습니다', main_div.text)
+        self.assertIn(post_000.title, main_div.text)
+        self.assertEqual(Post.objects.all().count(), 1)
+
+    def test_post_list_without_category(self):
+        post_000 = create_post(
+            title='the first post',
+            content='Hello World',
+            author=self.author_000,
+            created=timezone.now(),
+            modified=timezone.now(),
+        )
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        category_div = soup.find('div', id='side-div')
+
+        self.assertNotIn('django', category_div.text)
+        self.assertIn('기타', category_div.text)
+
+
+
+
     def test_post_create(self):
         category = create_category()
         post_000 = create_post(
@@ -94,6 +141,7 @@ class TestView(TestCase):
 
         print(post_000.created, post_000.modified)
         self.assertEqual('hihihi', post_000.title)
+
 
 
 
